13e573c95c75be85c9f7fdac347168555ed0c207
exodojaffar <exodojaffar@gmail.com>
2021-08-05 15:52:22 -0400

    fix: dir path for pem credentials

--- eadapi/sessionead.py
+++ eadapi/sessionead.py
@@ -19,7 +19,7 @@ class SessionEad(Session):
 		super(SessionEad, self).__init__()
 
 		self.headers.update({'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'})
-		self.verify = f"{DIR_NAME}/ead-ifms-edu-br-chain.pem"
+		self.verify = f"{DIR_NAME}/../ead-ifms-edu-br-chain.pem"
 
 		self._filter_data = getDataByDict
 
